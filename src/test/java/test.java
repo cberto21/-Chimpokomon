a6963035c5305da7f2d736d8c8dea9f1e36f0387
Claudio Molina <molina.claudio@estudiantes.unahur.edu.ar>
2022-09-11 16:58:31 -0300

    edito test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,24 +2,27 @@ import org.junit.jupiter.api.Assertions;
 
 public class test{
     public static void main(String[]args){
-       BuilderChimpoko builder = new BuilderChimpoko();
-       CreadorDirector creadorDirector = new CreadorDirector(builder);
+        BuilderChimpoko builder = new BuilderChimpoko();
+        CreadorDirector creadorDirector = new CreadorDirector(builder);
 
-       Chinpokomon carnotron = creadorDirector.crearCarnotron();
-       Chinpokomon zapato = creadorDirector.crearZapato();
+        Chinpokomon carnotron = creadorDirector.crearCarnotron();
+        Chinpokomon zapato = creadorDirector.crearZapato();
 
-       while (!carnotron.estaMuerto() && !zapato.estaMuerto()){
-           var numAtaqueA = NumAleatorio.aleatorio(zapato.listaDeAtaques.size() -1 );
-           var numAtaqueB = NumAleatorio.aleatorio(carnotron.listaDeAtaques.size() -1 );
-           zapato.atacarA(carnotron, zapato.listaDeAtaques.get(numAtaqueA));
-           carnotron.atacarA(zapato, carnotron.listaDeAtaques.get(numAtaqueB));
-           System.out.println(zapato.vidaActual);
-           System.out.println(carnotron.vidaActual);
-       }
-       if (carnotron.estaMuerto()) {
-           System.out.println(zapato.nombre + " Winner");
-       } else if (zapato.estaMuerto()) {
-           System.out.println(carnotron.nombre + " Winner");
-       }
+        Logger log = Logger.getInstancia();
+
+        while (!carnotron.estaMuerto() && !zapato.estaMuerto()){
+            var numAtaqueA = NumAleatorio.aleatorio(zapato.listaDeAtaques.size() -1 );
+            var numAtaqueB = NumAleatorio.aleatorio(carnotron.listaDeAtaques.size() -1 );
+            zapato.atacarA(carnotron, zapato.listaDeAtaques.get(numAtaqueA));
+            carnotron.atacarA(zapato, carnotron.listaDeAtaques.get(numAtaqueB));
+
+            log.debug( zapato.nombre +"VidaActual:"+ zapato.vidaActual );
+            log.debug( carnotron.nombre +"VidaActual:"+ carnotron.vidaActual );
+        }
+        if (carnotron.estaMuerto()) {
+            log.debug(zapato.nombre + " Winner");
+        } else if (zapato.estaMuerto()) {
+            log.debug(carnotron.nombre + " Winner");
+        }
     }
 }
